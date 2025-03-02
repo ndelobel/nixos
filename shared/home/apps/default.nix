8a7bc9f0ed3415b77d94b7207fd06d4b4d4fbcea
ndelobel <nathan.delobel@protonmail.com>
2025-03-02 20:34:09 +0100

    add git credential helper

--- shared/home/apps/default.nix
+++ shared/home/apps/default.nix
@@ -10,6 +10,9 @@
     enable = true;
     userName = "ndelobel";
     userEmail = "nathan.delobel@protonmail.com";
+    extraConfig = {
+      credential.helper = "${pkgs.git.override { withLibsecret = true; }}/bin/git-credential-libsecret";
+    };
   };
 
   home.packages = with pkgs; [
